ed74edaa27e1ef32367f60dab324c26c02029bd3
Isuru Fernando <isuruf@gmail.com>
2021-07-09 12:33:38 -0500

    Add boost example

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,3 +3,6 @@ set -e
 pushd pybind11_exception_rtti_test
 make test
 popd
+
+${CXX} boost_example.cpp -o test -I $CONDA_PREFIX/include $CONDA_PREFIX/lib/libboost_serialization.dylib -Wl,-rpath,$CONDA_PREFIX/lib
+./test
